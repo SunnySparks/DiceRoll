4b6129c08355a3263e34d68dff7ab63a4e3ec8e9
SunnySparks <josediazt6@outlook.com>
2020-02-04 12:29:07 -0600

    Finished
    Finished

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function rollDice(){
+	var die1 = document.getElementById("die1");
+	var die2 = document.getElementById("die2");
+	var status = document.getElementById("status");
+	var d1 = Math.floor(Math.random() * 6) +1;
+	var d2 = Math.floor(Math.random() * 6) +1;
+	var diceTotal = d1 + d2;
+	die1.innerHTML = d1;
+	die2.innerHTML = d2;
+	status.innerHTML = "You rolled "+diceTotal+".";
+	if(d1 === d2) {
+		status.innerHTML += " DOUBLES! You get a free turn!";
+	}
+}
